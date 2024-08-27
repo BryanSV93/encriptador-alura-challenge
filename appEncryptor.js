a2e6aa3ad1baee745a7f50823ed5a099814cbb07
bryansv93 <bryansanabriav@yahoo.es>
2024-08-26 20:24:06 -0600

    first commit

---
+++ appEncryptor.js
@@ -0,0 +1,95 @@
+
+
+let resultadoActual = ''
+
+//Funciones de encriptacion
+
+function encriptador2(textoRecibido2) {
+  console.log("EJEC ENCRIPTADOR - TEXTO SIN ENCRIPTAR: " + textoRecibido2);
+
+  arregloResultado = [];
+
+  for (let index = 0; index < textoRecibido2.length; index++) {
+    switch (textoRecibido2[index]) {
+      case "a":
+        arregloResultado.push("ai");
+        break;
+      case "e":
+        arregloResultado.push("enter");
+        break;
+      case "i":
+        arregloResultado.push("imes");
+        break;
+      case "o":
+        arregloResultado.push("ober");
+        break;
+      case "u":
+        arregloResultado.push("ufat");
+        break;
+      default:
+        arregloResultado.push(textoRecibido2[index]);
+        break;
+    }
+  }
+  // console.log(arregloResultado.join(""));
+  return arregloResultado.join("");
+}
+
+function desEncriptador2(textoRecibidoparaDesencriptar) {
+  resultadoDeDesencriptacion = textoRecibidoparaDesencriptar
+    .replace(/ai/g, "a")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+
+  //console.log(resultadoDeDesencriptacion);
+  return resultadoDeDesencriptacion;
+}
+
+// Funciones Generales
+
+function copiar() {
+copiarAlPortapapes()
+}
+
+function clickEncriptar() {
+  ocultarElementosSeccionResultado();
+  let resultadoEncriptacion = encriptador2(obtenerDatosInput());
+  modificarTextoResultado(resultadoEncriptacion);
+}
+
+function clickDesencriptar() {
+  ocultarElementosSeccionResultado();
+  let resultadoDescriptacion = desEncriptador2(obtenerDatosInput());
+  modificarTextoResultado(resultadoDescriptacion);
+}
+
+function obtenerDatosInput() {
+  return document.getElementById("idTextoIngresado").value;
+}
+
+function modificarTextoResultado(texto) {
+  resultadoActual = texto
+  let elementoHTML = document.getElementById("textoResultado");
+  elementoHTML.innerHTML = texto;
+  return;
+}
+
+function ocultarElementosSeccionResultado() {
+  document.getElementById("muneco").style.display = "none";
+  document.getElementById("textoInicialSeccionResultado1").style.display =
+    "none";
+  document.getElementById("textoInicialSeccionResultado2").style.display =
+    "none";
+}
+
+function copiarAlPortapapes() {
+  navigator.clipboard.writeText(resultadoActual).then(() => {
+    Toastify({
+      text: "Texto Copiado al Portapapeles",
+
+      duration: 3000,
+    }).showToast();
+  });
+}
